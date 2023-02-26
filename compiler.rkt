e09eb11ef97388197c64fe94202a471e8a89957c
taoufikcherroud@gmail.com <taoufikcherroud@gmail.com>
2023-02-26 19:59:36 +0100

    Implemented generate-x64 compiler pass

--- compiler.rkt
+++ compiler.rkt
@@ -45,7 +45,7 @@
 	   #:when (and (p64v1-binop? binop) (p64v1-register? reg1) (p64v1-register? reg2))
 	 `(set! ,reg1 (,binop ,reg1 ,reg2))]
  	 [ (reg1 `(,binop ,reg2 ,_))
-	   (error "Register require the same name!")])]
+	   (error (string-append "Register in instruction: "(~a exp1 exp2)  "require the same name!"))])]
   	[e (error (string-append "invalid instruction: " (~a e)))]))
 
 (p64v1-sequence '(set! rsp 15))
@@ -69,25 +69,19 @@
 
 
 
-
 ;; Optional; if you choose not to complete, implement a stub that returns the input
-
-
-
-;; 
-
 
 
 (define (check-paren-x64-init p)
   (define (initialized? reg inits)
     (and (member reg inits) #t))
-      (match p
+      (define initialized-regs (match p
 	   [`(begin ,s ...) 
 	(let check-initialized
     	((initialized '())
      	 (s s))
 	     (match s
-		    ['() p]
+		    ['() initialized]
 		    [`((set! ,_ (,binop ,reg ,_)) ,rest ...)
 
 		      (if (initialized? reg initialized)
@@ -97,6 +91,9 @@
 		    [`((set! ,reg ,_) ,rest ...)
 		      (check-initialized `(,reg ,@initialized) rest)]
 		    		    ))]))
+      (if (initialized? 'rax initialized-regs)
+	p
+	(error (string-append "Register rax not initialized: Please initialize rax in: " (~a p)))))
 
 
 (check-paren-x64-init
@@ -120,30 +117,89 @@
 (define (check-paren-x64 p)
   (check-paren-x64-init (check-paren-x64-syntax p)))
 
+
+(check-paren-x64 '(begin (set! 5 4)))
+
+
 ;; Optional; if you choose not to complete, implement a stub that returns a valid exit code
 (define (interp-paren-x64 p)
+  (define regs-hash (make-hash '((rsp . 0)(rbp . 0)(rax . 0)(rbx . 0)(rcx . 0)(rdx . 0)(rsi . 0)(rdi . 0)(r8 . 0)(r9 . 0)(r10 . 0)(r11 . 0)(r12 . 0)(r13 . 0)(r14 . 0) (r15 . 0))))
+  (define binop-hash (make-hash `((+ . ,x64-add) (* . ,x64-mul))))
+  (define (get-triv-value triv)
+    (if (p64v1-register? triv)
+      (dict-ref regs-hash triv)
+      triv))
   (define (eval-instruction-sequence regfile s)
-    (if (empty? s)
         ; If no more instructions, return exit code modulo 256 (since operating
-        ; systems return exit code modulo 256).
-        (modulo (dict-ref regfile 'rax) 256)
-        (TODO ...)))
-  (TODO ...))
+        ; systems return exit code modulo 256).	
+        (match s
+	       ['() (modulo (dict-ref regfile 'rax) 256)]
 
+
+	       [`((set! ,reg ,triv) ,rest ...)
+		#:when(or (p64v1-register? triv) (int64? triv))
+			(hash-set! regfile reg (get-triv-value triv))
+			(eval-instruction-sequence regfile rest)]
+	       [`((set! ,reg (,binop ,reg ,triv)) ,rest ...)
+		 (let*
+		   ((reg-val (get-triv-value reg))
+		    (triv-val (get-triv-value triv))
+		    (bop (dict-ref binop-hash binop))
+		    (new-value (bop reg-val triv-val)))
+		   (hash-set! regfile reg new-value))
+		 (eval-instruction-sequence regfile rest)]))
+  (match p 
+	 [`(begin ,s ...) (eval-instruction-sequence regs-hash s)]))
+
+
+(interp-paren-x64 '(begin (set! rdi 5)
+			  (set! rax 6)
+			  (set! rax (+ rax 5))))
 (define (generate-x64 p)
+  (define binop-hash (make-hash '((+ . "add") (* . "imul"))))
   ; Paren-x64-v1 -> x64-instruction-sequence
   (define (program->x64 p)
     (match p
       [`(begin ,s ...)
-       (TODO ...)]))
+       (let make-inst-seq
+	 ((s s)
+	  (inst-seq ""))
+	 (match s
+	      ['() inst-seq]
+	      [`((set! ,reg (,binop ,reg ,triv)) ,rest ...) (make-inst-seq rest (string-append inst-seq (binop->ins `(,binop ,reg ,triv))))] 
+	      [`((set! ,reg ,triv) ,rest ...) (make-inst-seq rest (string-append inst-seq (statement->x64 `(,reg ,triv))))]
+	      ))]))
+
+
+
+
 
   (define (statement->x64 s)
-    (TODO ... ))
+    (match s
+	   [`(,reg ,triv) (string-append "mov " (~a reg) "," (~a triv) "\n")]))
+
+
+
+
+
 
   (define (binop->ins b)
-    (TODO ... ))
+    (match b
+	   [`(,binop ,reg ,triv) (string-append (dict-ref binop-hash binop) " "  (~a reg) "," (~a triv)) "\n"]))
+
+
+
 
   (program->x64 p))
+
+(pretty-display (generate-x64 
+		  '(begin (set! rdi 5)
+			  (set! rax 6)
+			  (set! rax (+ rax 5)))))
+
+
+
+
 
 (define (wrap-x64-run-time str)
   (TODO ...))
