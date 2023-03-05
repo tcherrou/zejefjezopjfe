649c8080bc5d07773e213fcf4295bc97a9a23161
taoufikcherroud@gmail.com <taoufikcherroud@gmail.com>
2023-03-05 21:34:54 +0100

    Added tests and commentary

--- compiler.rkt
+++ compiler.rkt
@@ -21,9 +21,17 @@
 ;; int64? and int32? already implemented
 
 
+(module+ test
+  (require
+   rackunit
+   rackunit/text-ui
+   cpsc411/langs/v1
+   cpsc411/test-suite/public/v1))
 
 
 
+
+;; Helpers
 
 (define (p64v1-register? reg)
   (and (member reg '(rsp rbp rax rbx rcx rdx rsi rdi r8 r9 r10 r11 r12 r13 r14 r15)) #t))
@@ -34,6 +42,8 @@
   (and (member bop '(+ *)) #t))
 
 
+ 
+; p64v1-sequence: checks if a set! is correctly formed 
 
 (define (p64v1-sequence s)
   (match s 
@@ -57,7 +67,7 @@
 
 
 
-
+; p64v1-process : checks if paren-x64-v1 program starts with (begin ...) and the statements are correctly formed
 
 
 (define (p64v1-process p)
@@ -70,8 +80,7 @@
 
 
 
-
-;; Optional; if you choose not to complete, implement a stub that returns the input
+; check-paren-x64-init:  Check if all the registers used are initialized especially rax
 
 
 (define (check-paren-x64-init p)
@@ -99,17 +108,41 @@
 
 
 
-;; Optional; if you choose not to complete, implement a stub that returns the input
+
+(module+ test 
+	 (define-test-suite 
+	   check-paren-x64-init-test
+	   (check-not-exn (lambda ()(check-paren-x64-init '(begin (set! rax 0)))))
+	   (check-exn exn:fail? (lambda () (check-paren-x64-init '(begin (set! rax (+ rbx 4))))))))
+
+
+; check-paren-x64-syntax:  check if p is a correctly formed paren-x64-v1 program
+
 (define (check-paren-x64-syntax p)
   (p64v1-process p))
+
+
+
+
+(module+ test 
+	 (define-test-suite check-paren-x64-syntax-test
+	   (check-not-exn (lambda () (check-paren-x64-syntax '(begin (set! rcx rax) (set! rbx (+ rbx 5))))))
+	   (check-exn exn:fail? (lambda () (check-paren-x64-syntax '(begin (set! rbx '(* rax 5))))))))
+
+
+
+
+
+
+
+; check-paren-x64: checks if a paren-x64-v1 is well formed and initialized all register that were used
 
 (define (check-paren-x64 p)
   (check-paren-x64-init (check-paren-x64-syntax p)))
 
 
+; interp-paren-x64: interprets a paren-x64-v1 program
 
-
-;; Optional; if you choose not to complete, implement a stub that returns a valid exit code
 (define (interp-paren-x64 p)
   (define regs-hash (make-hash '((rsp . 0)(rbp . 0)(rax . 0)(rbx . 0)(rcx . 0)(rdx . 0)(rsi . 0)(rdi . 0)(r8 . 0)(r9 . 0)(r10 . 0)(r11 . 0)(r12 . 0)(r13 . 0)(r14 . 0) (r15 . 0))))
   (define binop-hash (make-hash `((+ . ,x64-add) (* . ,x64-mul))))
@@ -140,9 +173,11 @@
 	 [`(begin ,s ...) (eval-instruction-sequence regs-hash s)]))
 
 
+
+; generate-x64 generates the x64 instructions corresponding with the paren-x64-v1 program p
+
 (define (generate-x64 p)
   (define binop-hash (make-hash '((+ . "add") (* . "imul"))))
-  ; Paren-x64-v1 -> x64-instruction-sequence
   (define (program->x64 p)
     (match p
       [`(begin ,s ...)
@@ -156,24 +191,14 @@
 	      ))]))
 
 
-
-
-
   (define (statement->x64 s)
     (match s
 	   [`(,reg ,triv) (string-append "mov " (~a reg) "," (~a triv) "\n")]))
 
 
-
-
-
-
   (define (binop->ins b)
     (match b
 	   [`(,binop ,reg ,triv) (string-append (dict-ref binop-hash binop) " "  (~a reg) "," (~a triv)) "\n"]))
-
-
-
 
   (program->x64 p))
 
@@ -181,13 +206,33 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+; wrap-x64-run-time : add x64 instructions that moves a value in rax and performs a system call with the exit code as value of the program
+
+
 (define (wrap-x64-run-time str)
   (define run-time-string "\n exit: \n mov rdi, rax \n mov rax,60 \n syscall")
   (string-append str run-time-string))
 
+
+; wrap-x64-boiler-plate: add needed boilerplate to x64 program
+
 (define (wrap-x64-boilerplate str)
   (define boilerplate-string "global start \n section .text \n start: \n")
   (string-append  boilerplate-string str))
+
+
+; compile-paren-x64 : combines all above passes into one procedure
 
 (define (compile-paren-x64 p)
   (wrap-x64-boilerplate (wrap-x64-run-time (generate-x64 (check-paren-x64 p)))))
@@ -198,7 +243,11 @@
     generate-x64
     wrap-x64-run-time
     wrap-x64-boilerplate))
- (execute
+ 
+(module+ test 
+	 (define-test-suite correctness-test
+	   (check-equal? 
+	      (execute
    '(begin
       (set! rax 170679)
       (set! rdi rax)
@@ -207,26 +256,34 @@
       (set! rsp (* rsp rsp))
       (set! rbx 8991))
     nasm-run/exit-code)
+	      (interp-paren-x64
+   '(begin
+      (set! rax 170679)
+      (set! rdi rax)
+      (set! rdi (+ rdi rdi))
+      (set! rsp rdi)
+      (set! rsp (* rsp rsp))
+      (set! rbx 8991))
+)
+)))
 
 
-(module+ test
-  (require
-   rackunit
-   rackunit/text-ui
-   cpsc411/langs/v1
-   cpsc411/test-suite/public/v1)
 
-  (run-tests
-   (v1-public-test-suite
-    (list
-     check-paren-x64
-     generate-x64
-     wrap-x64-run-time
-     wrap-x64-boilerplate)
-    (list
-     interp-paren-x64-v1
-     interp-paren-x64-v1
-     #f
-     #f)
-    check-paren-x64
-    interp-paren-x64)))
+
+
+
+
+
+
+
+
+  (module+ test
+    (run-tests
+    (make-test-suite "Tests"
+      (list 
+      check-paren-x64-init-test
+      check-paren-x64-syntax-test
+      correctness-test
+
+      )
+    )))
